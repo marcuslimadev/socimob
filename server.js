fa24373dccf1d5d0ce89e032931040e091729740
Marcus Lima <marcus.lima@hotmail.com.br>
2026-01-20 17:34:26 -0300

    fix: Update Node.js server to use WHATWG URL API
    - Replaced deprecated url.parse() with modern URL constructor
    - Improved security and removed deprecation warnings
    - Maintained same functionality for serving static files

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const url = require('url');
 
 const PORT = 8000;
 const PUBLIC_DIR = path.join(__dirname, 'public');
 
 const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    let pathname = parsedUrl.pathname;
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    let pathname = url.pathname;
+
+    // Default to index.html for root
+    if (pathname === '/' || pathname === '') {
+        pathname = '/index.html';
+    }
+
+    // Handle portal routes
+    if (pathname.startsWith('/portal/')) {
+        pathname = '/portal' + pathname.substring(7);
+    }
+
+    const filePath = path.join(PUBLIC_DIR, pathname);
+
+    // Check if file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            // Try adding .html extension
+            const htmlPath = filePath + '.html';
+            fs.access(htmlPath, fs.constants.F_OK, (err2) => {
+                if (err2) {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('File not found');
+                    return;
+                }
+
+                serveFile(htmlPath, res);
+            });
+            return;
+        }
+
+        serveFile(filePath, res);
+    });
+});
 
     // Default to index.html for root
     if (pathname === '/' || pathname === '') {
