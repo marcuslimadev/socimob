354db243c87f143353a4dbeb83da5f8e0bfbb7d4
Marcus Lima <marcus.lima@hotmail.com.br>
2026-01-30 13:39:14 -0300

    feat: adicionar ObserverServiceProvider

---
+++ app/Providers/ObserverServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Models\Lead;
+use App\Observers\LeadObserver;
+
+class ObserverServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Lead::observe(LeadObserver::class);
+    }
+}
